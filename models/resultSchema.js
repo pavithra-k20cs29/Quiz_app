32d65d235d3e2995293b227722a3214a5c68bce9
pavithra-k20cs29 <pavithrakaruppusamy2003@gmail.com>
2024-01-19 15:35:16 +0530

    new

--- models/resultSchema.js
+++ models/resultSchema.js
@@ -7,9 +7,8 @@ const resultModel = new Schema({
     result : { type : Array , default : []},
     attempts : { type : Number , default : 0 },
     points : { type : Number, dafault : 0},
-    achived : { type : String , default : ''},
+    achieved : { type : String , default : ''},
     createdAt : { type : Date , default : Date.now}
 })
-
 
 export default mongoose.model('result', resultModel)
